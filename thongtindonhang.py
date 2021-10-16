9ed85d55f2574267acda642eb309281bfaab606c
ChuanHoang2021 <89921775+ChuanHoang2021@users.noreply.github.com>
2021-10-16 21:19:58 +0700

    thong tin don hang

---
+++ thongtindonhang.py
@@ -0,0 +1,9 @@
+so = int(input("Nhập giá: "))
+if so >= 150:
+	print("Tổng: ", so - 50)
+elif so >= 100:
+	print("Tổng: ", so - 25)
+elif so >= 75:
+	print("Tổng: ", so - 15)
+else:
+print("Tổng: ", so)
