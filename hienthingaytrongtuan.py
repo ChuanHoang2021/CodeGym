a499336e982c2a96c18f6bafb6768a85826e00be
ChuanHoang2021 <89921775+ChuanHoang2021@users.noreply.github.com>
2021-10-16 21:15:34 +0700

    hien thi ngay trong tuan

---
+++ hienthingaytrongtuan.py
@@ -0,0 +1,17 @@
+so = int(input("Nhap so tu 1 den 7: "))
+if so == 1:
+	print("Monday")
+elif so == 2:
+	print("Tuesday")
+elif so == 3:
+	print("Wednesday")
+elif so == 4:
+	print("Thursday")
+elif so == 5:
+	print("Friday")
+elif so == 6:
+	print("Saturday")
+elif so == 7:
+	print("Sunday")
+else:
+print("So da nhap ngoai 1-7")
